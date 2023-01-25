e8a98a5644587dcc3d9ef3f6d2063edded6b6d11
Kauan Vieira <kauanvieirakvs22@gmail.com>
2023-01-25 14:55:38 -0300

    routing e html e css do login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavegationComponent } from './components/navegation/navegation.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavegationComponent
+    NavegationComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
