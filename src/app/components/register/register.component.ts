2e8a8ddc1d9385ce62caeca649443a374a8fd1c0
Kauan Vieira <kauanvieirakvs22@gmail.com>
2023-01-26 11:09:58 -0300

    register finalizado - sem o http request

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -19,5 +19,19 @@ export class RegisterComponent {
 
   constructor(private formBuilder: FormBuilder) { }
 
+  transformInFormData(form: FormGroup): FormData {
+    let formData: FormData = new FormData();
+    formData.append('firstName', form.get('firstName')?.value);
+    formData.append('lastName', form.get('lastName')?.value);
+    formData.append('username', form.get('username')?.value);
+    formData.append('email', form.get('email')?.value);
+    formData.append('password', form.get('password')?.value);
+    formData.append('birth', form.get('birth')?.value);
+    return formData;
+  }
+
+  onSubmitRegister(): void {
+    let form: FormData = this.transformInFormData(this.formRegister);
+  }
 
 }
