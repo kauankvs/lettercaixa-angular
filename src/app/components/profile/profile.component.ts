6a1d1f6d53d5b86144d0300942e2757a4db366a2
Kauan Vieira <kauanvieirakvs22@gmail.com>
2023-01-29 16:20:11 -0300

    profile básico com http request funcionando

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Account } from 'src/app/interfaces/account';
 import { LetterboxApiMovieService } from 'src/app/services/letterbox-api-movie.service';
 import { LocalStorageService } from 'src/app/services/local-storage.service';
@@ -11,25 +12,26 @@ import { LocalStorageService } from 'src/app/services/local-storage.service';
 export class ProfileComponent implements OnInit {
   profile?: Account;
 
-  constructor(private service: LetterboxApiMovieService, private storageService: LocalStorageService) { }
+  constructor(private service: LetterboxApiMovieService, private storageService: LocalStorageService, private router: Router) { }
 
   async ngOnInit(): Promise<void | Error> {
     let token: string | null = this.storageService.getToken();
 
-    if(token == null)
-      return new Error('Null token!');
-    
-    this.service.getAccountRequest(token).subscribe({
-      next: (data) => {
-        console.log(data);
-        this.profile = data;
-      },
-      error: (err) => console.error(err)
-    });
-    await this.storageService.removeTokenAfterExpirationAsync();
+    if(token != null)
+    {
+      this.service.getAccountRequest(token).subscribe({
+        next: (data) => {
+          console.log(data);
+          this.profile = data;
+        },
+        error: (err) => console.error(err)
+      });
+      await this.storageService.removeTokenAfterExpirationAsync();
+    }
     
     if(this.profile == null) {
-      return new Error('Invalid token!');
+      window.alert('User not logged in!');
+      this.router.navigateByUrl('/login');
     }
   }
 
